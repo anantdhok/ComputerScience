357396e9f09ecf7eef6d0ca1802eb2550318e6ca
Anant Dhok <67429427+anantdhok@users.noreply.github.com>
2021-02-07 23:32:28 +0530

    Add files via upload

---
+++ Algorithms/Searching/BasicSearch.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Search(int key, vector<int> data) {
+    int i = 0;
+    while (i < data.size()) {
+        if (data[i] == key)
+            break;
+        i += 1;
+    }
+    return (data[i] == key) ? i : -1;
+}
+
+int main() {
+    int key = 4;
+    vector<int> data{ 1, 5, 2, 7, 4, 8, 3, 0, 9};
+    cout << "\n At index : " << Search(key, data);
+    return 0;
+}
